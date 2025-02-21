7be08571b5ef1db2aa59befe4e537d88b6373ac0
Paulo Henrique PErei <74430831+paulofreyyy@users.noreply.github.com>
2025-02-21 18:19:54 -0300

    feat: criando controller

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post } from '@nestjs/common';
+import { ItemsService } from './items.service';
+import { CreateItemDto } from './dtos/create-item.dto';
+import { Item } from './schemas/item.schema';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+    constructor(private readonly itemsService: ItemsService) { }
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    async create(@Body() body: CreateItemDto): Promise<Item> {
+        const item = await this.itemsService.create(body);
+        return item;
+    }
+
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    async findAll(): Promise<Item[]> {
+        return this.itemsService.findAll();
+    }
+
+    @Get(':id')
+    @HttpCode(HttpStatus.OK)
+    async findOne(@Param('id') id: string): Promise<Item> {
+        return this.itemsService.findOne(id);
+    }
+
+    @Get()
+    @HttpCode(HttpStatus.NO_CONTENT)
+    async update(@Body() body: CreateItemDto, @Param('id') id: string): Promise<Item> {
+        return this.itemsService.update(id, body);
+    }
+
+    @Get()
+    @HttpCode(HttpStatus.NO_CONTENT)
+    async remove(@Param('id') id: string): Promise<void> {
+        return this.itemsService.remove(id);
+    }
+}
